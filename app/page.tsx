4de6a5d8b0c63d8b42004ecd1c0ebf4366d80dbc
Ceasor7 <josegee0707@gmail.com>
2024-05-06 17:48:09 +0300

    page changes

--- app/page.tsx
+++ app/page.tsx
@@ -14,21 +14,23 @@ export default function Home() {
       <div className="h-[40rem] w-full rounded-md bg-gray-700 dark:bg-background relative flex flex-col items-center justify-center antialiased">
         <div className="max-w-2xl mx-auto p-4">
           <h1 className="relative z-10 text-lg md:text-7xl text-white bg-clip-text text-transparent  text-center font-bold">
-            Hello, I&apos;m Joseph
+            Welcome to CeasorCodes Org
           </h1>
-          <p></p>
           <p className="text-white max-w-lg mx-auto my-2 text-sm text-center relative z-10">
-            Dive into a world where each project is a story of passion and every
-            skill is a brushstroke on the canvas of professionalism. Explore,
-            engage, and be inspired as you wander through the digital corridors
-            of my personal portfolio.
+            The hub for cutting-edge web and software development solutions. Our
+            organization specializes in crafting bespoke web applications,
+            offering comprehensive software development services, and providing
+            expert consultations to elevate your digital presence. Dive into our
+            weekly blog for the latest insights on technology trends and sports
+            updates, all curated by our in-house experts to keep you informed
+            and ahead of the curve.
           </p>
           <div className="flex flex-col gap-4 justify-center sm:flex-row">
             <Link
               href="/blog"
               className={cn(buttonVariants({ size: 'lg' }), 'w-full sm:w-fit')}
             >
-              View my blog
+              View blog
             </Link>
             <Link
               href={siteConfig.links.github}
